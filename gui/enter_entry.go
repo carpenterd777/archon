d2ece19a74b7bb67d51155ed25627cf0f449a3bf
David Carpenter <carpenterd777@gmail.com>
2021-07-18 00:06:58 -0400

    added enter entry type

---
+++ gui/enter_entry.go
@@ -0,0 +1,41 @@
+package gui
+
+import (
+	"time"
+
+	"fyne.io/fyne/v2"
+	"fyne.io/fyne/v2/widget"
+	"github.com/archon/backend"
+)
+
+// An Entry field that submits some text when the Enter key is pressed while this is focused. Multiline by default.
+type EnterEntry struct {
+	widget.Entry
+	session *backend.Session // a session state that this entry is allowed to modify
+}
+
+// Handler for enter key presses. Clears the text in the entry.
+func (e *EnterEntry) onEnter() {
+	e.session.AddNote(backend.NewNote(e.Text, time.Now()))
+	e.Entry.SetText("")
+}
+
+// Overrides the TypedKey method of the fyne.Focusable interface.
+func (e *EnterEntry) TypedKey(key *fyne.KeyEvent) {
+	switch key.Name {
+	case fyne.KeyEnter:
+		e.onEnter()
+	case fyne.KeyReturn:
+		e.onEnter()
+	default:
+		e.Entry.TypedKey(key)
+	}
+}
+
+// Creates a new Entry that adds a note to the passed session when Enter is pressed.
+func NewEnterEntry(session *backend.Session) *EnterEntry {
+	entry := &EnterEntry{session: session}
+	entry.Entry.MultiLine = true
+	entry.ExtendBaseWidget(entry)
+	return entry
+}
